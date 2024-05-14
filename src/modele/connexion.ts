94862cb50023c66bc2db9f96530d75577667ad2c
Mathieu Kuhnapfel <e9486u@iutm-inf-f36-14.iutmetz.site.univ-lorraine.fr>
2024-05-14 09:45:44 +0200

    :construction:(connexion.ts): mise à jour SQL

--- src/modele/connexion.ts
+++ src/modele/connexion.ts
@@ -8,7 +8,7 @@ class Connexion {
 	}
 	init():void {
 		// à adapter avec votre nom de base et vos identifiants de connexion
-		APIsql.sqlWeb.bdOpen('devbdd.iutmetz.univ-lorraine.fr','3306','projet_bdinventaire', 'projet_ihm','butinfoprojetihm', 'utf8')
+		APIsql.sqlWeb.bdOpen('devbdd.iutmetz.univ-lorraine.fr','3306','e9486u_SAE201', 'e9486u_appli','32314477', 'utf8')
 	}
 }
 let connexion = new Connexion;
