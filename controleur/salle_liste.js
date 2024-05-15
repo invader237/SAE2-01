997c369757267eab83ac886a15404d9cc7972c66
invader237 <diego.trivino.fr@gmail.com>
2024-05-15 10:06:58 +0200

    :wastebasket:(/controleur, /modele): deleting JavaScript files

--- controleur/salle_liste.js
+++
@@ -1,6 +0,0 @@
-import { vueSalleListe } from "../controleur/class_salle_liste.js";
-vueSalleListe.init({ divTitre: document.querySelector('[id=div_salle_liste_titre]'),
-    btnAjouter: document.querySelector('[id=btn_salle_ajouter]'),
-    tableSalle: document.querySelector('[id=table_salle]')
-});
-//# sourceMappingURL=salle_liste.js.map
