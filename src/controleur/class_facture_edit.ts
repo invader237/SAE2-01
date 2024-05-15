2a4a3d58e95409b7fc872ebb73abe2c623ada0e1
PAGES Guillaume <guillaume.pages8@etu.univ-lorraine.fr>
2024-05-15 11:38:52 +0200

    :construction:(class_facture_edit.ts): Adding Select option, and beginning client info

--- src/controleur/class_facture_edit.ts
+++ src/controleur/class_facture_edit.ts
@@ -1,5 +1,4 @@
-import { DonneListe }	from "./class_facture_liste"
-
+import { UneFacture } from "../modele/data_facture"
 
 type TFactureEditForm = { 
 	divDetail:HTMLElement, 
@@ -13,7 +12,7 @@ type TFactureEditForm = {
 	btnRetour:HTMLInputElement, 
     btnValider:HTMLInputElement, 
     btnAnnuler:HTMLInputElement,
-	lblDetailDept:HTMLLabelElement,
+	lblDetailClient:HTMLLabelElement,
 	lblNumErreur :HTMLLabelElement, 
     lblEtageErreur:HTMLLabelElement, 
     lblDeptErreur:HTMLLabelElement, 
@@ -32,79 +31,66 @@ type TFactureEditForm = {
     lblQteErreur:HTMLLabelElement,
 }
 
-class DonneListe {
-    private _numero: number;
-    private _date: Date;
-    private _client: number;
-    private _nomClient: string;
-    private _prix: number;
-    private _remise: number;
-    private _livraison: number;
+type TStatutValeur = 'correct' | 'vide' | 'inconnu' | 'doublon'
 
-    constructor(numero: number, date: Date, client: number, nomClient: string,prix:number, remise: number, livraison: number) {
-        this._numero = numero;
-        this._date = date;
-        this._client = client;
-        this._nomClient = nomClient;
-        this._prix = prix;
-        this._remise = remise;
-        this._livraison = livraison;
-    }
+type TErreur 		=  { 
+	statut : TStatutValeur, 
+	msg:{ [key in TStatutValeur] : string } 
+}
 
-    avecRemise():number{
-        return this._prix-this._prix*this._remise
-    }
 
-    get numero():number{
-        return this._numero
-    }
 
-    get date():Date{
-        return this._date
-    }
+class UnClient{
+	private _id   : string;
+	private _civ  : string;
+	private _nom   :string;
+	private _prenom  : string;
+	private _mel  : string;
+	private _adr  : string;
+	private _cp  : string;
+	private _commune  : string;
+	private _remiseMax  : string;
 
-    get client():number{
-        return this._client
-    }
+	constructor(id: string, civ: string, nom: string, prenom: string, mel: string, adr: string,cp: string, commune : string, remiseMax : string ){
+		this._id= id
+		this._civ= civ 
+		this._nom= nom 
+		this._prenom= prenom 
+		this._mel= mel 
+		this._adr= adr 
+		this._cp= cp 
+		this._commune= commune 
+		this._remiseMax= remiseMax
+	}
 
-    get nomClient(): string{
-        return this._nomClient
-    }
-    
-    get prix():number{
-        return this._prix
-    }
+	get id():string{
+		return this._id
+	}
 
-    get remise():number{
-        return this._remise
-    }
-
-    get livraison():number{
-        return this._livraison
-    }
-
-    set numero(numero:number){
-        this._numero=numero
-    }
-    set date(date:Date){
-        this._date=date
-    }
-    set client(client:number){
-        this._client=client
-    }
-    set nomClient(nomClient:string){
-        this._nomClient=nomClient
-    }
-    set prix(prix:number){
-        this._prix=prix
-    }
-    set remise(remise:number){
-        this._remise=remise
-    }
-    set livraison(livraison:number){
-        this._livraison=livraison
-    }
-
+	get civ():string{
+		return this._civ
+	}
+	get nom():string{
+		return this._nom
+	}
+	get prenom():string{
+		return this._prenom
+	}
+	get mel():string{
+		return this._mel
+	}
+	get adr():string{
+		return this._adr
+	}
+	get cp():string{
+		return this._cp
+	}
+	get commune():string{
+		return this._commune
+	}
+	get remiseMax():string{
+		return this._remiseMax
+	}
 }
 
 class UnProduit {
@@ -154,9 +140,12 @@ class UnProduit {
 
 }
 
+
 class VueFactureEdit {
 	private _form 		: TFactureEditForm
 	private _unProduit: UnProduit
+	private _unClient: UnClient
+	private _uneFacture: UneFacture
 	private _params 	: string[];		// paramètres reçus par le fichier HTML 
 										// tel que  params[0] :  mode affi, modif, suppr, ajout
 										// 			params[1] : id en mode affi, modif, supp
@@ -167,21 +156,53 @@ class VueFactureEdit {
 	get params(): string[]{ 
         return this._params	
     }
+	private _erreur		: {	// tableau contenant les messages d'erreur pour chaque type d'erreur pour chaque zone de saisie à vérifier
+		[key:string] : TErreur 
+	}
+	get erreur()  :{[key:string]:TErreur}	{ return this._erreur	}
 
+	initMsgErreur():void { // les erreurs "champ vide", "valeur inconnue", "doublon" sont les trois principales erreurs dans un formulaire
+		// pour chaque champ à contrôler (événement onChange), création des 3 messages d'erreur + message pour correct
+		// avec chaîne vide si pas d'erreur générée pour un type d'erreur potentielle
+			this._erreur = { edtNum		: {statut :'vide', msg:{correct:"", vide:"Le numéro d'identifications doit être renseigné."		 ,inconnu:"Le numéro d'identification ne peut contenir que des chiffres."						,doublon:"Le numéro d'identification est déjà attribué."} }
+							,edtEtage	: {statut :'vide', msg:{correct:"", vide:"La date doit être renseigné."					 ,inconnu:""						,doublon:""} }
+							,edtCodeClient: {statut :'vide', msg:{correct:"", vide:"Le numéro du client doit être renseigné."			 ,inconnu:"Client inconnu."	,doublon:""} }
+							,equipt		: {statut :'vide', msg:{correct:"", vide:"La facture doit contenir au moins un produit.",inconnu:""						,doublon:""} }
+							,listeEquipt: {statut :'vide', msg:{correct:"", vide:"Aucun produit choisi"								,inconnu:""	,doublon:""} }
+							,edtQte		: {statut :'vide', msg:{correct:"", vide:"La quantité doit être un nombre entier supérieur à 0"	,inconnu:""	,doublon:""} }}
+		}
 
 	init(form:TFactureEditForm) {
 		this._form=form
-		alert("1")
 		this.form.listeContenue.style.display = "none";
 		this.form.edtContenueQte.style.display = "none";
+		this._uneFacture=new UneFacture("1", "2024-05-23", "1", "BOZZO","75", "10", "30")
 		this._unProduit= new UnProduit(27,"Evian","eau en bouteille",0.5,8,10)
+		this._unClient = new UnClient("1","M.","BOZZO","Raoul","bozzo.raoul@gmail.com", "24 rue du cirque","57000","Metz","20")
 		this.affichageListe();
-		this.ajouterProduit(28,"Christaline","bouteille en plastique",0.75,5,1 )
+		this.initMsgErreur();
 		this.form.btnAjouterFacture.onclick = function():void{
 			vueFactureEdit.afficherFactureEdit();
 		}
+		this.form.btnRetour.onclick = function():void { 
+			vueFactureEdit.retourClick(); 
+		}  
+		const affi = this.params[0] === 'affi';
+		if (this.params[0] !== 'ajout')
+		{	// affi ou modif ou suppr
+			this.form.edtNum.value 			= this._uneFacture.numero;			
+			this.form.edtLib.value			= this._uneFacture.nomClient;
+			this.form.edtDate.value		= this._uneFacture.date;
+			this.form.edtClient.value		= this._unClient.id;
+			this.form.edtNum.readOnly 		= true;
+			this.form.edtLib.readOnly 		= affi;
+			this.form.edtClient.readOnly 	= affi;
+			this.erreur.edtNum.statut 		= "correct";
+			this.detailClient();
+		}
 	}
 	afficherFactureEdit():void {
+		this.affichageProduit()
 		this.form.listeContenue.style.display = "block";
 		this.form.edtContenueQte.style.display = "block";
 		this.form.btnAjouterFacture.hidden = false;
@@ -201,11 +222,63 @@ class VueFactureEdit {
 		tr.insertCell().textContent = this._unProduit.condProduit.toString();
 		tr.insertCell().textContent = this._unProduit.puProduit.toString();
 		tr.insertCell().textContent = this._unProduit.qteProduit.toString();
-		tr.insertCell().textContent = this._unProduit.montant().toString();
+		tr.insertCell().textContent = this._unProduit.montant().toString();	
 	}
 
-		
+	affiGrilleProduit():void {
+		const tr = this.form.tableContenue.insertRow();
+		let balisea : HTMLAnchorElement; // déclaration balise <a>
+				// création balise <a> pour appel modification équipement dans salle
+				balisea = document.createElement("a")
+				balisea.classList.add('img_modification')
+				balisea.onclick = function():void { vueFactureEdit.modifierEquiptClick(id); }
+				tr.insertCell().appendChild(balisea)
+				// création balise <a> pour appel suppression équipement dans salle
+				balisea = document.createElement("a")
+				balisea.classList.add('img_corbeille')
+				balisea.onclick = function():void { vueFactureEdit.supprimerEquiptClick(id); }
+				tr.insertCell().appendChild(balisea)
+	}
+
+	affichageProduit(): void {
+		//const option = this.form.tableContenue.options();
+		//option.insertCell().textContent = this._unProduit.libProduit;
+		this._form.listeContenue.options.add(new Option( this._unProduit.libProduit, this._unProduit.codeProduit.toString()));
+	}
+
+	detailClient(valeur : string):void {
+		const err = this.erreur.edtCodeClient
+		const detail   = this.form.lblDetailClient;
+		detail.textContent = "";		
+		err.statut = "correct";
+		const chaine : string = valeur.trim();
+		if (chaine.length > 0) {
+			//const dept : UnDept = lesDepts.byCodeDept(chaine); 
+			if (this._unClient.id !== "") {	// client trouvé 
+				detail.textContent 
+				= this._unClient +"\r\n" +this._unClient.civ+this._unClient.nom+this._unClient.prenom+this._unClient.adr+this._unClient.cp+this._unClient.commune+this._unClient.remiseMax; 
+			}
+			else { 
+				err.statut = 'inconnu';
+				detail.textContent = err.msg.inconnu;	
+			}
+		}
+		else err.statut = 'vide';		
+	}
+	
+	retourClick():void {
+		location.href = "salle_liste.html";		
+	}	
+
+	modifierEquiptClick(id : string):void {
+		this.afficherFactureEdit();		
+		this.form.listeContenue.length = 0;
+		this.form.listeContenue.options.add(new Option(this._unProduit.libProduit, this._unProduit.codeProduit.toString()));	// text, value = 0;
+		this.form.listeContenue.selectedIndex = 0;
+	}	
+
 }
+
 let vueFactureEdit = new VueFactureEdit;
 export { vueFactureEdit };
 //# sourceMappingURL=class_salle_edit.js.map
