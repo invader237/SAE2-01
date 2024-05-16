e33a7c91b594a2cbd93456343a7e5b36d8175926
invader237 <diego.trivino.fr@gmail.com>
2024-05-16 17:04:09 +0200

    :sparkles:(data_facture.ts): added edit, delete and modify methods

--- src/modele/data_facture.ts
+++ src/modele/data_facture.ts
@@ -3,15 +3,17 @@ import { APIsql } from "../modele/connexion.js"
 class UneFacture {
     private _numero: string;
     private _date: string;
+    private _commentFact: string;
     private _client: string;
     private _nomClient: string;
     private _prix: string;
     private _remise: string;
     private _livraison: string;
 
-    constructor(numero: string, date: string, client: string, nomClient: string, prix: string, remise: string, livraison: string) {
+    constructor(numero: string, date: string, commentFact: string, client: string, nomClient: string, prix: string, remise: string, livraison: string) {
         this._numero = numero;
         this._date = date;
+        this._commentFact = commentFact;
         this._client = client;
         this._nomClient = nomClient;
         this._prix = prix;
@@ -29,6 +31,10 @@ class UneFacture {
 
     get date(): string {
         return this._date
+    }
+
+    get commentFact(): string {
+        return this._commentFact
     }
 
     get client(): string {
@@ -51,12 +57,19 @@ class UneFacture {
         return this._livraison
     }
 
+    //set
+
     set numero(numero: string) {
         this._numero = numero
     }
     set date(date: string) {
         this._date = date
     }
+
+    set commentFact(commentFact: string) {
+        this._commentFact = commentFact;
+    }
+
     set client(client: string) {
         this._client = client
     }
@@ -78,7 +91,7 @@ class UneFacture {
         // pour un affichage dans une ligne d’un tableau HTML
         const tableau: APIsql.TtabAsso = {
             'numero': this._numero, 'date': this._date,
-            'client': this._client, 'nomClient': this._nomClient, 
+            'client': this._client, 'nomClient': this._nomClient,
             'prix': this._prix, 'remise': this._remise, 'livraison': this._livraison
         };
         return tableau;
@@ -99,6 +112,7 @@ class LesFactures {
         let sql = `SELECT
         f.num_fact, 
         f.date_fact, 
+        f.comment_fact,
         f.id_cli,
         (SELECT nom_cli FROM client c WHERE c.id_cli = f.id_cli) as nom_cli, 
         (SELECT SUM(l.qte_prod * p.tarif_ht) FROM ligne l, produit p WHERE l.num_fact = f.num_fact AND l.code_prod = p.code_prod) as prix,
@@ -126,7 +140,7 @@ class LesFactures {
         let factures: TFactures = {};
         for (let i = 0; i < result.length; i++) {
             const item: APIsql.TtabAsso = result[i];
-            const facture = new UneFacture(item['num_fact'], item['date_fact'], item['id_cli'],
+            const facture = new UneFacture(item['num_fact'], item['date_fact'], item['comment_fact'] ,item['id_cli'],
                 item["nom_cli"], item["prix"], item["taux_remise_fact"], item["livraison"]);
             factures[facture.numero] = facture;	// clé d’un élément du tableau : num facture
         }
@@ -134,7 +148,7 @@ class LesFactures {
     }
 
 
-    all(): TFactures {	// renvoie le tableau d’objets contenant toutes les salles
+    all(): TFactures {	// renvoie le tableau d’objets contenant toutes les factures
         return this.load(APIsql.sqlWeb.SQLloadData(this.prepare(""), []));
     }
 
@@ -145,6 +159,22 @@ class LesFactures {
             T.push(factures[id].toArray());
         }
         return T;
+    }
+
+    delete(num_fact: string): boolean {	// requÃªte de suppression dâ€™une facture dans la table
+        let sql = "DELETE FROM facture WHERE num_fact = ?";
+        return APIsql.sqlWeb.SQLexec(sql, [num_fact]);		// requÃªte de manipulation : utiliser SQLexec
+    }
+
+    insert(facture: UneFacture): boolean {	// requÃªte dâ€™ajout dâ€™une facture dans la table				// requÃªte de manipulation : utiliser SQLexec
+        let sql = "INSERT INTO facture(num_fact,date_fact,comment_fact,taux_remise_fact,id_cli,id_forfait) VALUES(?, ?, ?, ?, ?, ?)";
+        return APIsql.sqlWeb.SQLexec(sql, [facture.numero, facture.date,facture.commentFact,facture.remise,facture.client,facture.livraison]);
+    }
+
+    update(facture: UneFacture): boolean {	// requÃªte de modification dâ€™une facture dans la table
+        let sql = "UPDATE facture SET date_fact = ?, comment_fact = ?, taux_remise_fact = ?, id_cli = ?, id_forfait = ? ";
+        sql += " WHERE	num_facture	= ?"; 					// requÃªte de manipulation : utiliser SQLexec
+        return APIsql.sqlWeb.SQLexec(sql, [facture.date, facture.commentFact, facture.remise, facture.client, facture.livraison]);
     }
 
 }
