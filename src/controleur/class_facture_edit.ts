489c9ee27a93514bd44dd5fa580272920473b905
PAGES Guillaume <guillaume.pages8@etu.univ-lorraine.fr>
2024-05-16 17:08:29 +0200

    :poop:(class_facture_edit.ts): Adding a feature to choose type of delivery

--- src/controleur/class_facture_edit.ts
+++ src/controleur/class_facture_edit.ts
@@ -1,6 +1,7 @@
 import { UneFacture } from "../modele/data_facture"
 import { UnClient,DesClients,TClients } from "../modele/data_client"
 import { UnProduit } from "../modele/data_produit"
+import { UneLivraison } from "../modele/data_livraison"
 
 
 
@@ -11,7 +12,7 @@ type TFactureEditForm = {
     edtLib:HTMLInputElement, 
     edtDate:HTMLInputElement, 
     edtClient:HTMLInputElement,
-	edtLivraison: HTMLElement,
+	edtLivraison: HTMLTableElement,
 	edtRemise: HTMLInputElement,
 	btnRetour:HTMLInputElement, 
     btnValider:HTMLInputElement, 
@@ -48,6 +49,7 @@ class VueFactureEdit {
 	private _unProduit: UnProduit
 	private _unClient: UnClient
 	private _uneFacture: UneFacture
+	private _uneLivraison: UneLivraison
 	private _params 	: string[];		
 	private _erreur	: {	[key:string] : TErreur }							
 
@@ -79,9 +81,10 @@ class VueFactureEdit {
 		this._uneFacture=new UneFacture("1", "2024-05-23", "1", "BOZZO","75", "10", "30")
 		this._unProduit= new UnProduit("27","Evian","eau en bouteille","0.5","8","10")
 		this._unClient = new UnClient("1","M.","BOZZO","Raoul","bozzo.raoul@gmail.com", "24 rue du cirque","57000","Metz","20")
-		
-
+		this._uneLivraison = new UneLivraison("STD","livraison standard", "mt_forfait")
+	
 		this.affichageListe();
+		this.selectLivraison();
 		this.initMsgErreur();
 
 		this.form.edtClient.onchange = function():void{
@@ -118,6 +121,7 @@ class VueFactureEdit {
 
 	affichageListe():void{
 		const tr = this.form.tableContenue.insertRow();
+		//a supp
 		let balisea : HTMLAnchorElement; // dÃ©claration balise <a>
 		// crÃ©ation balise <a> pour appel page visualisation du dÃ©tail de la salle
 		balisea = document.createElement("a")
@@ -153,8 +157,9 @@ class VueFactureEdit {
 	}
 
 	detailClient():void {
+		// a réecrire/améliorer
 		const desClient = new DesClients();
-		const data_client :TClients = desClient.all();
+		const data_client :TClients = desClient.all(); 
 		const detail = this.form.lblDetailClient;
 		const valeur = vueFactureEdit.form.edtClient.value 
 		const detailClient = data_client[valeur];
@@ -164,6 +169,14 @@ class VueFactureEdit {
 			= detailClient["civ"] +" "+ detailClient["nom"] +" "+ detailClient["prenom"]+"\r\n" + detailClient["adr"] + " - " + detailClient["cp"]+" "+ detailClient["commune"]+"\r\n" + detailClient["mel"]+ "\r\n" +"taux de remise maximum accordé : "+ detailClient["remiseMax"]+"%"; 
 		}
 	}
+
+	selectLivraison(){
+		const tr = this.form.edtLivraison.insertRow();
+
+		tr.insertCell().textContent = this._uneLivraison.idForfait;
+		tr.insertCell().textContent = this._uneLivraison.libForfait;
+		tr.insertCell().textContent = this._uneLivraison.mtForfait;
+	}
 	
 	retourClick():void {
 		location.href = "salle_liste.html";		
@@ -172,7 +185,7 @@ class VueFactureEdit {
 	modifierEquiptClick(id : string):void {
 		this.afficherFactureEdit();		
 		this.form.listeContenue.length = 0;
-		this.form.listeContenue.options.add(new Option(this._unProduit.libProduit, this._unProduit.codeProduit.toString()));	// text, value = 0;
+		this.form.listeContenue.options.add(new Option(this._unProduit.nom, this._unProduit.code.toString()));	// text, value = 0;
 		this.form.listeContenue.selectedIndex = 0;
 	}	
 
