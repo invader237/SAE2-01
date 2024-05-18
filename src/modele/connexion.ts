7cc1c80f8839f85217e511bc465c19bd210c87cb
invader237 <diego.trivino.fr@gmail.com>
2024-05-18 20:06:04 +0200

    :card_file_box:(connexion.ts): database change

--- src/modele/connexion.ts
+++ src/modele/connexion.ts
@@ -1,6 +1,6 @@
 import * as APIsql from "../modele/sqlWeb.js"
 
-APIsql.sqlWeb.init("https://devweb.iutmetz.univ-lorraine.fr/~e9486u/ihm/SAE/vue/",
+APIsql.sqlWeb.init("https://devweb.iutmetz.univ-lorraine.fr/~trivino7u/ihm/SAE/vue/",
 						"https://devweb.iutmetz.univ-lorraine.fr/~nitschke5/ihm/IHM_API/")
 class Connexion {
 	constructor() {
@@ -8,7 +8,7 @@ class Connexion {
 	}
 	init():void {
 		// à adapter avec votre nom de base et vos identifiants de connexion
-		APIsql.sqlWeb.bdOpen('devbdd.iutmetz.univ-lorraine.fr','3306','e9486u_SAE201', 'e9486u_appli','32314477', 'utf8')
+		APIsql.sqlWeb.bdOpen('devbdd.iutmetz.univ-lorraine.fr','3306','trivino7u_SAE2-01', 'trivino7u_appli','leadiego', 'utf8')
 	}
 }
 let connexion = new Connexion;
